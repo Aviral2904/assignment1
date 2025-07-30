1582410cfa0ae8580207b95a193f988ae0f6ea13
Aviral Mishra <aviralmishra@Avirals-MacBook-Pro.local>
2025-07-30 16:28:54 +0530

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+
+
+//Write a Swift program to create a new array with the elements in reverse order of a given array of integers.
+
+
+func reverseArray(array : [Int]) -> [Int]{
+    
+    var reversedArray : [Int] = []
+    
+    reversedArray = array.reversed()
+    
+    return reversedArray
+}
+
+
+var array1 = reverseArray(array: [2,4,6,8,10])
+
+print(array1)
